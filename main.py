8dd882db29350a50a27c1ff2db40d13da219893e
Akudze <139904778+Akudze05@users.noreply.github.com>
2023-12-18 18:23:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import tkinter
 import server
 
+s = [] # list config
+
 class App:
-    def __init__(self, *args):
+    def __init__(self, *args): # basic dates
         self.size = (800, 600)
         self.title = 'Window'
         self.icon = 'path./'
 
-    def run(self, region, status):
+    def run(self, region, status): # set start pos
         self.region = region
         self.status = status
 
     def config(self, **kwargs):
-        pass
-
+        for i in kwargs: # sorting in list
+            s.pop[i]
+            
 if __name__ == '__main__':
     app = App
     app.run()
