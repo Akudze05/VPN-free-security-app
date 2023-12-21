d472ef4168d97c028202c9edcd7e045e45b14b84
Akudze <139904778+Akudze05@users.noreply.github.com>
2023-12-21 20:02:16 +0300

    Update main.py
    create connection to server

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import tkinter
 import server
 
-s = [] # list config
-
 class App:
-    def __init__(self, *args): # basic dates
+    def __init__(self, *args):
         self.size = (800, 600)
         self.title = 'Window'
         self.icon = 'path./'
 
-    def run(self, region, status): # set start pos
+    def run(self,to_region, region, status):
         self.region = region
+        self.to_region = to_region
         self.status = status
 
+        if region != to_region:
+            status = True
+            while status:
+                start()
+                if status == False:
+                    server.exit()
+        else:
+            print("Нет подключения. Выберите другую страну!")
+
     def config(self, **kwargs):
-        for i in kwargs: # sorting in list
-            s.pop[i]
-            
+        pass
+
 if __name__ == '__main__':
     app = App
     app.run()
+
